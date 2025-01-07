8e5f3a083c9e8d09e6ca5ae3485d8bab560d51aa
Username387J <jdeleijagbulu@gmail.com>
2025-01-06 19:56:43 -0700

    Add files via upload

---
+++ homework.py
@@ -0,0 +1,17 @@
+set1 = set([])
+numberoftimes = int(input("How many values do you intend to add?"))
+
+for i in range (numberoftimes) :
+    num = int(input("Insert the variable: "))
+    set1.add(num)
+
+print(set1)
+
+set2 = set([])
+number = int(input("How many values?"))
+
+for i in range(number):
+    nom = int(input("Inset the variable: "))
+    set2.add(nom)
+
+print(set2)
--- main.py
+++ main.py
@@ -24,3 +24,44 @@ print(set1)
 #set1.remove(500)
 set1.discard(500)
 print(set1)
+
+#Set operations
+# 01) Union
+# 02) Intersection
+# 03) Difference
+# 04) Symmetric difference
+
+print("\n")
+
+a = {1, 2, 3, 4, 5,}
+b = { 4, 5, 6, 7, 8}
+
+#Union means addition of sets
+print("Set a : ",a)
+print("Set b: ",b)
+print(a.union(b))#the same as print(a | b)
+
+#Intersection means the common elements between two sets
+print("\n")
+print("Intersection of two sets")
+print(a.intersection(b))
+print(a & b)
+
+#Gives only the unique values in the first value; Difference of A and B is the elements that exist in a but not b
+print("\n")
+print("difference of A and B")
+print(a.difference(b))
+print(a - b)
+
+#Difference of B and a is the elements that exist in b but not a
+print("\n")
+print("difference between b and a")
+print(b.difference(a))
+print(b - a)
+
+#Symmetric difference is the union of sets - intersection of sets
+#removes common terms
+print("\n")
+print("Symmetrical difference between a and b")
+print(a.symmetric_difference(b))
+print(a ^ b)
