6369b45cf6d467db0ca2418421c8012608f199f4
Username387J <jdeleijagbulu@gmail.com>
2025-01-11 20:44:02 -0700

    Update homework.py

--- homework.py
+++ homework.py
@@ -15,3 +15,7 @@ for i in range(number):
     set2.add(nom)
 
 print(set2)
+print("if set 1 is",set1,", and set 2 is",set2,"then the symmetric difference would be",set1.symmetric_difference(set2),",")
+print("the union of both sets would result in",set1.union(set2),",")
+print("the difference between the 1st and second set would be",set1.difference(set2),"thus vice versa would be equal to",set2.difference(set1))
+print("and the intersection would result in", set1.intersection(set2))
